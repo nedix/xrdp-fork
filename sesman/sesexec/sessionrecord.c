neutrinolabs/xrdp

More g_* functions usage Follow coding standard

sesman/sessionrecord.c
@@ -60,27 +60,29 @@ add_xtmp_entry(int pid, const char *line, const char *user, const char *rhostnam
      *
      * So the IP is the string up the two last colons
      */
-    int i = strlen(rhostname) - 1;
-    while ((i>0) && (rhostname[i] != ':')) {
+    int i = g_strlen(rhostname) - 1;
+    while ((i>0) && (rhostname[i] != ':'))
+    {
         i--;
     }
     i--;
-    while ((i>0) && (rhostname[i] != ':')) {
+    while ((i>0) && (rhostname[i] != ':'))
+    {
         i--;
     }
 
     hostname = g_strndup(rhostname, i);
 
-    memset(&ut, 0, sizeof(ut));
+    g_memset(&ut, 0, sizeof(ut));
 
     ut.ut_type=state;
     ut.ut_pid = pid;
     gettimeofday(&tv, NULL);
     ut.ut_tv.tv_sec = tv.tv_sec;
     ut.ut_tv.tv_usec = tv.tv_usec;
-    strncpy(ut.ut_line, line , sizeof(ut.ut_line));
-    strncpy(ut.ut_user, user , sizeof(ut.ut_user));
-    strncpy(ut.ut_host, hostname, sizeof(ut.ut_host));
+    g_strncpy(ut.ut_line, line , sizeof(ut.ut_line));
+    g_strncpy(ut.ut_user, user , sizeof(ut.ut_user));
+    g_strncpy(ut.ut_host, hostname, sizeof(ut.ut_host));
 
     /* utmp */
     setutxent();

nedix/xrdp-fork

sesman/sesexec/sessionrecord.c
@@ -38,6 +38,7 @@
 
 #include "log.h"
 #include "os_calls.h"
+#include "string_calls.h"
 #include "sessionrecord.h"
 
 /*
@@ -51,16 +52,16 @@ add_xtmp_entry(int pid, const char *line, const char *user, const char *rhostnam
     _utmp ut;
     struct timeval tv;
 
-    memset(&ut, 0, sizeof(ut));
+    g_memset(&ut, 0, sizeof(ut));
 
     ut.ut_type=state;
     ut.ut_pid = pid;
     gettimeofday(&tv, NULL);
     ut.ut_tv.tv_sec = tv.tv_sec;
     ut.ut_tv.tv_usec = tv.tv_usec;
-    strncpy(ut.ut_line, line , sizeof(ut.ut_line));
-    strncpy(ut.ut_user, user , sizeof(ut.ut_user));
-    strncpy(ut.ut_host, rhostname, sizeof(ut.ut_host));
+    g_strncpy(ut.ut_line, line , sizeof(ut.ut_line));
+    g_strncpy(ut.ut_user, user , sizeof(ut.ut_user));
+    g_strncpy(ut.ut_host, rhostname, sizeof(ut.ut_host));
 
     /* utmp */
     setutxent();